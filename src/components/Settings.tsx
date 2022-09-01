47fff093587288b1701d5c8296d2c4849f1db4bc
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-01 22:20:23 +0200

    Add colors

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,19 +1,27 @@
-import { Box, Grid, Slider, Typography } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Grid,
+  IconButton,
+  Slider,
+  Typography,
+} from "@mui/material";
+import PlayIcon from "../assets/play.png";
 
 /**
  * Settigns should control
  * 1. Width & height of the universe ☑️
  * 2. Probability of being a live ☑️
- * 3. Reproduction speed
- * 4. Colors & images
+ * 3. Reproduction speed ☑️
+ * 4. Colors
  * 5. Pause & continue
  */
 const Settings = () => {
   return (
     <Grid
       container
-      spacing={2}
+      spacing={4}
       sx={{
         width: "100%",
         backgroundColor: "#f9fafb",
@@ -21,9 +29,9 @@ const Settings = () => {
         alignContent: "center",
       }}
     >
-      <Grid item xs={2}>
+      <Grid item xs={4}>
         <Box>
-          <Typography variant="h6">Width</Typography>
+          <Typography variant="h6">Width 📏</Typography>
           <Slider
             aria-label="Temperature"
             defaultValue={30}
@@ -35,7 +43,7 @@ const Settings = () => {
           />
         </Box>
         <Box>
-          <Typography variant="h6">Height</Typography>
+          <Typography variant="h6">Height 📏</Typography>
           <Slider
             aria-label="Temperature"
             defaultValue={30}
@@ -47,7 +55,7 @@ const Settings = () => {
           />
         </Box>
         <Box>
-          <Typography variant="h6">Probability of being alive</Typography>
+          <Typography variant="h6">Probability of being alive 🧬</Typography>
           <Slider
             defaultValue={0.5}
             valueLabelDisplay="auto"
@@ -55,6 +63,44 @@ const Settings = () => {
             min={0}
             max={1}
           />
+        </Box>
+      </Grid>
+
+      <Grid
+        item
+        xs={4}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <IconButton>
+          <img src={PlayIcon} alt="start" />
+        </IconButton>
+      </Grid>
+
+      <Grid item xs={4}>
+        <Box>
+          <Typography variant="h6">Reproduction speed 🚅</Typography>
+          <Slider
+            defaultValue={10}
+            valueLabelDisplay="auto"
+            step={2}
+            min={0}
+            max={100}
+          />
+        </Box>
+
+        <Box>
+          <Typography mb="8px" variant="h6">
+            Colors
+          </Typography>
+          <ButtonGroup variant="outlined" aria-label="outlined button group">
+            <Button>One</Button>
+            <Button>Two</Button>
+            <Button>Three</Button>
+          </ButtonGroup>
         </Box>
       </Grid>
     </Grid>
