5b94b1fc53f5f6d5cbedd326279221dc564fac08
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-01 21:35:21 +0200

    Render animation frame

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -12,36 +12,45 @@ const Canvas = () => {
   const ref = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    renderUniverse();
+    gameLoop();
   }, [ref]);
 
-  async function renderUniverse() {
+  async function gameLoop() {
     if (!ref.current) return;
     let height = 40;
     let width = 40;
     try {
+      let canvas = ref.current;
       let universe: Universe = await invoke("new_universe", {
         height,
         width,
       });
 
-      let canvas = ref.current;
-      canvas.height = (CELL_SIZE + 1) * height + 1;
-      canvas.width = (CELL_SIZE + 1) * width + 1;
+      async function renderUniverse() {
+        canvas.height = (CELL_SIZE + 1) * height + 1;
+        canvas.width = (CELL_SIZE + 1) * width + 1;
 
-      let ctx = ref.current.getContext("2d");
+        let ctx = canvas.getContext("2d");
 
-      if (ctx === null) {
-        alert("Unable to draw the universe!");
-        return;
+        if (ctx === null) {
+          alert("Unable to draw the universe!");
+          return;
+        }
+
+        drawGrid(ctx, height, width);
+        drawCells(ctx, width, height, universe.cells);
+
+        universe = await invoke("next_gen", { universe });
+
+        requestAnimationFrame(renderUniverse);
       }
 
-      drawGrid(ctx, height, width);
-      drawCells(ctx, width, height, universe.cells);
+      await renderUniverse();
     } catch (error) {
       alert(error);
     }
   }
+
   return (
     <div>
       <canvas ref={ref} />
